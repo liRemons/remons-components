6cdd1f9ed95199868a69185fc76d1c2bf19caf17
liRemons <remons@foxmail.com>
2022-07-17 19:35:40 +0800

    test

--- src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from 'E:/job/project/remons-components/node_modules/umi/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu',],
-});
-
-export { plugin };
