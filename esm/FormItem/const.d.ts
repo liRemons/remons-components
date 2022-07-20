b0dd4abd6d54a1763895e5fdc695054c32d2ff37
liRemons <remons@foxmail.com>
2022-07-21 00:17:06 +0800

    Updates

--- esm/FormItem/const.d.ts
+++
@@ -1,6 +0,0 @@
-import { ReactElement } from 'react';
-interface ComType {
-    [propName: string]: React.ReactNode & ReactElement & any;
-}
-export declare const Com: ComType;
-export {};
--- esm/index.js
+++
@@ -1,9 +0,0 @@
-export { default as FormItem } from "./FormItem";
-export { default as SearchForm } from "./SearchForm";
-export { default as RangeInput } from "./RangeInput";
-export { default as SizeInput } from "./SizeInput";
-export { default as ActionList } from "./ActionList";
-export { default as ButtonBar } from "./ButtonBar";
-export { default as ToolBar } from "./ToolBar";
-export { default as Descriptions } from "./Descriptions";
-export { default as Mentions } from "./Mentions";
